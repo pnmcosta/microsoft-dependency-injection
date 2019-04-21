8a16285e9ec72078ee97d1b9880feaa11a52627b
Eugene Sadovoi <evgeni@eniks.com>
2019-04-21 19:01:19 -0400

    Fixed more tests

--- tests/UnityDependencyInjectionTests.cs
+++ tests/UnityDependencyInjectionTests.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Specification;
+using Microsoft.Extensions.DependencyInjection.Specification.Fakes;
+using Xunit;
 
 namespace Unity.Microsoft.DependencyInjection.Tests
 {
@@ -10,5 +14,54 @@ namespace Unity.Microsoft.DependencyInjection.Tests
         {
             return serviceCollection.BuildServiceProvider();
         }
+
+
+        [Fact]
+#pragma warning disable xUnit1024 // Test methods cannot have overloads
+        public new void DisposesInReverseOrderOfCreation()
+#pragma warning restore xUnit1024 // Test methods cannot have overloads
+        {
+            // Arrange
+            var serviceCollection = new TestServiceCollection();
+            serviceCollection.AddSingleton<FakeDisposeCallback>();
+            serviceCollection.AddTransient<IFakeOuterService, FakeDisposableCallbackOuterServiceFixed>();
+            serviceCollection.AddSingleton<IFakeMultipleService, FakeDisposableCallbackInnerService>();
+            serviceCollection.AddScoped<IFakeMultipleService, FakeDisposableCallbackInnerService>();
+            serviceCollection.AddTransient<IFakeMultipleService, FakeDisposableCallbackInnerService>();
+            serviceCollection.AddSingleton<IFakeService, FakeDisposableCallbackInnerService>();
+            var serviceProvider = CreateServiceProvider(serviceCollection);
+
+            var callback = serviceProvider.GetService<FakeDisposeCallback>();
+            var outer = serviceProvider.GetService<IFakeOuterService>();
+
+            // Act
+            ((IDisposable)serviceProvider).Dispose();
+
+            // Assert
+            Assert.Equal(outer, callback.Disposed[0]);
+            Assert.Equal(outer.MultipleServices.Reverse(), callback.Disposed.Skip(1).Take(3).OfType<IFakeMultipleService>());
+            Assert.Equal(outer.SingleService, callback.Disposed[4]);
+        }
+
+        internal class TestServiceCollection : List<ServiceDescriptor>, IServiceCollection
+
+        {
+
+        }
+    }
+
+    public class FakeDisposableCallbackOuterServiceFixed : FakeDisposableCallbackService, IFakeOuterService
+    {
+public FakeDisposableCallbackOuterServiceFixed(
+    IFakeService singleService,
+    IEnumerable<IFakeMultipleService> multipleServices,
+    FakeDisposeCallback callback) : base(callback)
+{
+    SingleService = singleService;
+    MultipleServices = multipleServices.ToArray();
+}
+
+        public IFakeService SingleService { get; }
+        public IEnumerable<IFakeMultipleService> MultipleServices { get; }
     }
 }
