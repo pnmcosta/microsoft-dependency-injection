93a1b7c225832e6115f46d616b009ea421fadf5c
Pedro Costa <pedro.costa@strata3.com>
2019-10-24 15:31:05 +0100

    diagnostic extension breaks behaviour

---
+++ tests/UnityDiagnosticTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Specification;
+
+namespace Unity.Microsoft.DependencyInjection.Tests
+{
+    public class UnityDiagnosticTests : DependencyInjectionSpecificationTests
+    {
+        readonly IUnityContainer container = new UnityContainer().AddExtension(new Diagnostic());
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+        {
+            return container.BuildServiceProvider(serviceCollection);
+        }
+    }
+}
---
+++ tests/UnityTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Specification;
+
+namespace Unity.Microsoft.DependencyInjection.Tests
+{
+    public class UnityTests : DependencyInjectionSpecificationTests
+    {
+        readonly IUnityContainer container = new UnityContainer();
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+        {
+            return container.BuildServiceProvider(serviceCollection);
+        }
+    }
+}
